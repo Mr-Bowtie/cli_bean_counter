1cd6e98d3ab2c16270ed7d453d4910f8abcbc6d2
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-09-17 16:36:28 -0400

    feat: sum all bills v1

--- bean_counter.rb
+++ bean_counter.rb
@@ -1,9 +1,16 @@
 require "yaml"
+require "pry"
 
 bills = YAML.load_file('bills.yml')
 
 
-puts bills["bills"].reduce(0){|memo,bill|
-  puts bill["amount"]
-  memo += bill["amount"]
-}
+# iterate through each bill group, then iterate through each bill in that group
+# add each bills dollar amount to a memo object
+memo = 0 
+bills.each do |type| 
+  type[1].each {|bill| 
+    memo += bill["amount"]}
+end
+puts memo
+
+
