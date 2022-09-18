6b6a2a07b117e2b47410f3e4a430828e0a2d644c
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-09-17 20:44:18 -0400

    refactor: move yaml files to config folder, add config file

--- bean_counter.rb
+++ bean_counter.rb
@@ -3,11 +3,14 @@ require "pry"
 require "date"
 
 class BeanCounter 
-  @@bill_file = "bills.yml"
-  attr_accessor :bills
+  @@bill_file = "config/bills.yml"
+  @@config_file = "config/config.yml"
+  attr_accessor :bills, :config, :pay_range
   
   def initialize()
     @bills = YAML.load_file(@@bill_file)
+    @config = YAML.load_FILE(@@config_file)
+    @pay_range = @config["pay_range"]
   end
 
   # iterate through each bill group, then iterate through each bill in that group
@@ -29,14 +32,16 @@ class BeanCounter
     end
 
     # create an array of all the bills with due dates in the date range
-    bills_to_pay = @bills["bills"].select{|bill| date_numbers.include?(bill["date_number"])}
+    bills_to_pay = @bills["monthly_bills"].select{|bill| date_numbers.include?(bill["date_number"])}
     
     # sum the payments 
     bills_to_pay.reduce(0) {|memo, bill| memo += bill["amount"]}
   end
 end
 
-range = Date.today..( Date.today + 14)
+def 
+
+  range = Date.today..( Date.today + bean_counter.pay_range)
 bean_counter = BeanCounter.new
 p bean_counter.sum_bills_in_period(range)
 
