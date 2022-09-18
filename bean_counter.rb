66c4760d6657311cce86e0b2e2e7d387d61e6da9
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-09-17 20:34:12 -0400

    feat: first pass at summing the bills in a given period

--- bean_counter.rb
+++ bean_counter.rb
@@ -1,5 +1,6 @@
 require "yaml"
 require "pry"
+require "date"
 
 class BeanCounter 
   @@bill_file = "bills.yml"
@@ -8,6 +9,7 @@ class BeanCounter
   def initialize()
     @bills = YAML.load_file(@@bill_file)
   end
+
   # iterate through each bill group, then iterate through each bill in that group
   # add each bills dollar amount to a memo object
   def sum_all 
@@ -19,10 +21,23 @@ class BeanCounter
     memo
   end 
 
-  def sum_bills_in_period()
 
+  def sum_bills_in_period(date_range)
+    date_numbers = []
+    for date in date_range
+      date_numbers.push(date.day)
+    end
+
+    # create an array of all the bills with due dates in the date range
+    bills_to_pay = @bills["bills"].select{|bill| date_numbers.include?(bill["date_number"])}
+    
+    # sum the payments 
+    bills_to_pay.reduce(0) {|memo, bill| memo += bill["amount"]}
   end
 end
 
-puts BeanCounter.new().sum_all
+range = Date.today..( Date.today + 14)
+bean_counter = BeanCounter.new
+p bean_counter.sum_bills_in_period(range)
+
 
