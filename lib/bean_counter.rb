4438895833800b4c60b321b99d238eeee5526959
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-09-26 06:50:52 -0400

    refactor: extract display methods to module, make methods reusable

--- lib/bean_counter.rb
+++ lib/bean_counter.rb
@@ -2,20 +2,23 @@ require "yaml"
 require "rainbow/refinement"
 require "pry"
 require "date"
+require "require_all"
+require_all "lib"
 using Rainbow 
 
 class App 
+  include Display
   @@bill_file = "config/bills.yml"
   @@config_file = "config/config.yml"
   attr_accessor :bills, :config, :pay_range, :start_date, :date_range, :paycheck
   
-  def initialize(date)
+  def initialize(date = Date.today.to_s)
     @bills = YAML.load_file(@@bill_file)
     @config = YAML.load_file(@@config_file)
     @pay_range = @config["pay_range"]
     @paycheck = @config["default_paycheck"]
     @start_date = parse_start_date(date)
-    # @date_range = @start_date..(@start_date + @pay_range)
+    @date_range = @start_date..(@start_date + @pay_range)
   end
 
   # iterate through each bill group, then iterate through each bill in that group
@@ -35,10 +38,11 @@ class App
     Date.parse(date)
   end
 
-  def date_range 
-    @start_date..(@start_date + @pay_range)
-  end
+  # def date_range 
+  #   @start_date..(@start_date + @pay_range)
+  # end
 
+  # TODO: get relavant categories from instance variable, instead of hardcoding which ones to display.  
   def gather_bills_in_period
     date_numbers = []
     for date in date_range
@@ -54,41 +58,6 @@ class App
     gather_bills_in_period.reduce(0) {|memo, bill| memo += bill["amount"]}
   end
 
-  def display_bills_in_range
-    CLI::UI::Frame.open("Bills") do 
-      gather_bills_in_period.each do |bill|
-        display_bill(bill)
-      end
-      puts "Total due:".red + " #{sum_bills_in_period}"
-    end 
-  end
-
-  def display_bills(scope) 
-    puts "-------------------------------------------".green
-    if scope == "all"
-      @bills.each do |type, bills|
-        bills.each do |bill|
-          display_bill(bill)
-          puts "-------------------------------------------".green
-        end
-      end
-    else 
-      @bills[scope].each do |bill|
-        display_bill(bill)
-        puts "-------------------------------------------".green
-      end
-    end
-  end
-
-  def display_bill(bill)
-    display_string = "Name:".red + " #{bill['name']}, " + "Amount Due:".blue + " #{bill['amount']}"
-    if bill["date_number"]
-      display_string += " Date:".yellow + " #{bill['date_number']}" 
-    else 
-      display_string += " Date:".yellow + " Pay every check"
-    end
-    puts display_string
-  end
 end
 
 
---
+++ lib/display.rb
@@ -0,0 +1,35 @@
+require "rainbow/refinement"
+using Rainbow 
+module Display
+  def display_bills_in_period
+    display_bills(gather_bills_in_period, date_range.to_s)
+  end
+
+  def display_bills_by_category(cat)
+    if cat == 'all'
+      bills.each do |type, bill_list|
+        display_bills(bill_list, type)
+      end
+    else 
+      display_bills(bills[cat], cat)
+    end
+  end
+
+  def display_bills(bill_arr, title) 
+    CLI::UI::Frame.open(title) do 
+      bill_arr.each do |bill|
+        display_bill(bill)
+      end
+    end
+  end
+
+  def display_bill(bill)
+    display_string = "Name:".red + " #{bill['name']}, " + "Amount Due:".blue + " #{bill['amount']}"
+    if bill["date_number"]
+      display_string += " Date:".yellow + " #{bill['date_number']}" 
+    else 
+      display_string += " Date:".yellow + " Pay every check"
+    end
+    puts display_string
+  end
+end
