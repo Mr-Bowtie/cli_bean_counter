b6a79b85d530197c7f977408984e7f3a625a2bf0
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-10-30 16:19:57 -0400

    feat: helper method to reload bills yaml file after an update

--- lib/bean_counter.rb
+++ lib/bean_counter.rb
@@ -94,6 +94,10 @@ class BeanCounter
   def calculate_division(div, lump)
     ( lump * ( div["percentage"].to_f / 100) ).round(2)    
   end
+   
+  def reload_bills 
+    @bills = YAML.load_file(@@bill_file)
+  end
 
 end
 
